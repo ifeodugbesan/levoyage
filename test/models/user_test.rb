e75280501a3e7903050ef76c21f8ff5e5cec8479
Ife Odugbesan <ifeodugbesan@yahoo.com>
2024-08-09 16:53:45 +0100

    testing unit and integration tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user has signed up through github omniauth" do
+    user = User.new(provider: "github")
+    assert(user.signed_in_with_github?, true)
+  end
 end
