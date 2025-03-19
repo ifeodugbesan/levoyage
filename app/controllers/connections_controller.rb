6e50e0163d60c92b79479740a221fbc7a9cc76c8
Ife Odugbesan <ifeodugbesan@yahoo.com>
2025-03-19 23:17:06 +0000

    removed the reverse

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -3,7 +3,7 @@ class ConnectionsController < ApplicationController
   before_action :connections_and_words, only: [:show, :check_guess]
 
   def index
-    @connections = policy_scope(Connection).select { |c| c.groups.count == 4 }.sort_by { |c| c.created_at }.reverse
+    @connections = policy_scope(Connection).select { |c| c.groups.count == 4 }.sort_by { |c| c.created_at }
     @connections = Kaminari.paginate_array(@connections).page(params[:page]).per(10)
   end
 
