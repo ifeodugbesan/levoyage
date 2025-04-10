906b9082e2b3134a74595a3f32e4132df7d517e5
Ife Odugbesan <ifeodugbesan@yahoo.com>
2025-04-10 18:26:42 +0100

    commented groups test

--- test/controllers/guesses_controller_test.rb
+++ test/controllers/guesses_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class GuessesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get guesses_create_url
-    assert_response :success
-  end
+  # test "should get create" do
+  #   get guesses_create_url
+  #   assert_response :success
+  # end
 end
