5869f65e53c90c48520d3fd72f1df6bcc271746b
Ife Odugbesan <ifeodugbesan@yahoo.com>
2025-04-10 18:30:14 +0100

    commented rest of the generated tests

--- test/controllers/connections_controller_test.rb
+++ test/controllers/connections_controller_test.rb
@@ -1,38 +1,38 @@
 require "test_helper"
 
 class ConnectionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get connections_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get connections_index_url
+  #   assert_response :success
+  # end
 
-  test "should get show" do
-    get connections_show_url
-    assert_response :success
-  end
+  # test "should get show" do
+  #   get connections_show_url
+  #   assert_response :success
+  # end
 
-  test "should get new" do
-    get connections_new_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get connections_new_url
+  #   assert_response :success
+  # end
 
-  test "should get create" do
-    get connections_create_url
-    assert_response :success
-  end
+  # test "should get create" do
+  #   get connections_create_url
+  #   assert_response :success
+  # end
 
-  test "should get edit" do
-    get connections_edit_url
-    assert_response :success
-  end
+  # test "should get edit" do
+  #   get connections_edit_url
+  #   assert_response :success
+  # end
 
-  test "should get update" do
-    get connections_update_url
-    assert_response :success
-  end
+  # test "should get update" do
+  #   get connections_update_url
+  #   assert_response :success
+  # end
 
-  test "should get destroy" do
-    get connections_destroy_url
-    assert_response :success
-  end
+  # test "should get destroy" do
+  #   get connections_destroy_url
+  #   assert_response :success
+  # end
 end
